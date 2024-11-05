c81db5ac4890823df947d26f4149f604238f85dd
EC2 Default User <ec2-user@ip-172-31-42-58.ap-northeast-1.compute.internal>
2024-11-05 12:02:21 +0000

    管理者ログイン機能

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
-  before_action :authenticate_admin!
+  
+  layout 'admin'
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -26,9 +27,9 @@ class Admin::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
   def after_sign_in_path_for(resource)
-    root_path
+    admin_homes_path
   end
   def after_sign_out_path_for(resource)
-    root_path
+    new_admin_session_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  
+
   devise_for :admins, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
   }
-  
+  namespace :admin do
+   get 'homes', to: 'homes#top'
+  end
+
   devise_for :customers, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
